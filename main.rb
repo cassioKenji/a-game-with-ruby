774b2cb172507978dac1d4b48acf024e99f8330b
Cássio Kenji <cassio.oshiro@walmart.com>
2016-11-30 17:25:40 -0200

    add the basics for a main class for the main loop and mains window draw

--- main.rb
+++ main.rb
@@ -1,16 +1,24 @@
 require 'gosu'
+require 'yaml'
+require 'pry'
 
 # game's main class
 class Main < Gosu::Window
   def initialize
-    super 640, 480
-    self.caption = 'A Game tih Gosu'
+    @config = YAML.load_file("project_settings.yml")
+    super 640, 480 # , :fullscreen => true
+    self.caption = @config[:game_name]
+    @bg = Gosu::Image.new("#{@config[:images][:bg]}space_bg.png", :tileable => true)
+    @font = Gosu::Font.new(20)
   end
 
   def update
   end
 
   def draw
+    @bg.draw(0, 0, 0)
+
+    @font.draw(@config, 10, 10, 10, 1.0, 1.0, 0xff_ffff00)
   end
 end
 
