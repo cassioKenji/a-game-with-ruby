625db7a1adf2cac47c07c274fbaf2e48b4b12ebe
Cássio Kenji <cassio.oshiro@walmart.com>
2016-12-02 18:56:35 -0200

    add player to the scene

--- main.rb
+++ main.rb
@@ -1,25 +1,34 @@
 require "gosu"
 require "yaml"
 require "pry"
+require_relative "class/player"
 
 # game's main class
 class Main < Gosu::Window
   def initialize
-    @config = YAML.load_file("project_settings.yml")
     super 640, 480 # , :fullscreen => true
-    self.caption = @config[:game_name]
-    @bg = Gosu::Image.new("#{@config[:images][:bg]}space_bg.png",
+    @settings = YAML.load_file("project_settings.yml").freeze
+    self.caption = @settings[:game_name]
+    @bg = Gosu::Image.new("#{@settings[:images][:bg]}space_bg.png",
                           tileable: true)
     @font = Gosu::Font.new(20)
+    @player = Player.new
   end
 
   def update
+    if Gosu::button_down? Gosu::KbLeft or Gosu::button_down? Gosu::GpLeft then
+      @player.turn_left
+    end
+    if Gosu::button_down? Gosu::KbRight or Gosu::button_down? Gosu::GpRight then
+      @player.turn_right
+    end
   end
 
   def draw
     @bg.draw(0, 0, 0)
 
-    @font.draw(@config, 10, 10, 10, 1.0, 1.0, 0xff_ffff00)
+    @player.draw
+    @font.draw(@settings, 10, 10, 10, 1.0, 1.0, 0xff_ffff00)
   end
 end
 
